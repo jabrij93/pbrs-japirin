0cf5a89c74a08ff46d18bdf44e55baf004a9c024
jabss <jabrijuhinin93@gmail.com>
2025-10-10 14:20:30 +0800

    update list pengalaman

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -15,12 +15,12 @@ export default function About() {
           <h3 className="font-semibold">Pengalaman</h3>
 
           <ul className="mt-2 text-sm text-gray-700 list-disc list-inside space-y-1 text-justify">
+            <li>Ketua Bahagian Dun Kundasang, Parti Bersatu Rakyat Sabah (PBRS), 2018-sekarang</li>
             <li>Setiausaha Politik kepada Menteri, Kementerian Sains, Teknologi, Inovasi dan Ekonomi (MOSTI), 2013-2016</li>
             <li>Setiausaha Politik kepada Menteri, Kementerian Pembangunan Luar Bandar Negeri Sabah (KPLB), 2008-2013</li>
-            <li>Setiausaha Politik kepada Menteri, Kementerian Pembangunan Perindustrian Negeri Sabah (MID), 2004-2008</li>
-            <li>Ketua Bahagian Dun Kundasang, Parti Bersatu Rakyat Sabah (PBRS), 2018-sekarang</li>
             <li>Timbalan Ketua Bahagian UPKO Ranau, United Progressive Kinabalu Organisation, 2008-2013</li>
             <li>Ketua Pemuda Barisan Nasional Ranau, Barisan Nasional, 2008-2013</li>
+            <li>Setiausaha Politik kepada Menteri, Kementerian Pembangunan Perindustrian Negeri Sabah (MID), 2004-2008</li>
             <li>Ketua Pemuda UPKO Bahagian Ranau, United Progressive Kinabalu Organisation, 2003-2008</li>
           </ul>
 
